32dddd904be5aecc52cf1afc483325936250a2ee
Mateus Andrade <mat.andrade@live.com>
2020-03-09 21:21:24 -0300

    added private routes logic

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 import SignIn from '~/pages/Signin';
 import SignUp from '~/pages/Signup';
@@ -7,14 +7,18 @@ import SignUp from '~/pages/Signup';
 import Dashboard from '~/pages/Dashboard';
 import Profile from '~/pages/Profile';
 
+import Route from './Route';
+
 export default function Routes() {
     return (
         <Switch>
             <Route path="/" exact component={SignIn} />
             <Route path="/register" component={SignUp} />
 
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/profile" component={Profile} />
+            <Route path="/dashboard" component={Dashboard} isPrivate />
+            <Route path="/profile" component={Profile} isPrivate />
+
+            {/* <Route path="/" component={() => <h1>404</h1>} /> */}
         </Switch>
     );
 }
