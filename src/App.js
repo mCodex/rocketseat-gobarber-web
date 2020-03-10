305f8bf12316c271dee36931c4dbec956d83baab
Mateus Andrade <mat.andrade@live.com>
2020-03-10 20:54:45 -0300

    added code splitting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 import Routes from './routes';
 
 import history from '~/services/history';
 
+import GlobalStyle from './styles/global';
+
 import '~/config/ReactotronConfig';
 
 const App = () => (
     <Router history={history}>
+        <Helmet>
+            <link
+                href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap"
+                rel="stylesheet"
+            />
+        </Helmet>
+        <GlobalStyle />
         <Routes />
     </Router>
 );
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { lazy,Suspense } from 'react';
 import { Switch } from 'react-router-dom';
+// import SignIn from '~/pages/Signin';
+// import SignUp from '~/pages/Signup';
 
-import SignIn from '~/pages/Signin';
-import SignUp from '~/pages/Signup';
-
-import Dashboard from '~/pages/Dashboard';
-import Profile from '~/pages/Profile';
+// import Dashboard from '~/pages/Dashboard';
+// import Profile from '~/pages/Profile';
 
 import Route from './Route';
+
+const SignIn = lazy(() => import('~/pages/Signin'));
+const SignUp = lazy(() => import('~/pages/Signup'));
+
+const Dashboard = lazy(() => import('~/pages/Dashboard'));
+const Profile = lazy(() => import('~/pages/Profile'));
 
 export default function Routes() {
     return (
         <Switch>
-            <Route path="/" exact component={SignIn} />
-            <Route path="/register" component={SignUp} />
+            <Suspense fallback={<div/>}>
+                <Route path="/" exact component={SignIn} />
+                <Route path="/register" component={SignUp} />
 
-            <Route path="/dashboard" component={Dashboard} isPrivate />
-            <Route path="/profile" component={Profile} isPrivate />
-
+                <Route path="/dashboard" component={Dashboard} isPrivate />
+                <Route path="/profile" component={Profile} isPrivate />
+            </Suspense>
             {/* <Route path="/" component={() => <h1>404</h1>} /> */}
         </Switch>
     );
