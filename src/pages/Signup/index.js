5be045cdc1d014ee0d7f8ba99b56b2423b7bc0a6
Mateus Andrade <mat.andrade@live.com>
2020-03-11 19:29:03 -0300

    added auth validation

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+    name: Yup.string().required('Nome é obrigatório'),
+    email: Yup.string()
+        .email('Insira um e-mail válido')
+        .required('O e-mail é obrigatório'),
+    password: Yup.string()
+        .min(6, 'Mínimo de 6 caracteres')
+        .required('A senha é obrigatória'),
+});
 
 const SignUp = () => {
     const handleSubmit = data => {
@@ -12,7 +23,7 @@ const SignUp = () => {
     return (
         <>
             <img src={logo} alt="GoBarber" />
-            <Form onSubmit={handleSubmit}>
+            <Form schema={schema} onSubmit={handleSubmit}>
                 <Input name="name" placeholder="Nome Completo" />
                 <Input type="email" name="email" placeholder="Seu e-mail" />
                 <Input
