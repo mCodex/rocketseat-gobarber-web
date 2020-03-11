4dad9f7eb9aa642c106bb6a4cedfe4d145d9a679
Mateus Andrade <mat.andrade@live.com>
2020-03-11 19:23:40 -0300

    added unform

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 const Signin = () => {
+    const handleSubmit = data => {
+        console.tron.log(data);
+    };
+
     return (
         <>
             <img src={logo} alt="GoBarber" />
-            <form action="post">
-                <input type="email" name="email" placeholder="Seu e-mail" />
-                <input
+            <Form onSubmit={handleSubmit}>
+                <Input type="email" name="email" placeholder="Seu e-mail" />
+                <Input
                     type="password"
                     name="password"
                     placeholder="Sua senha"
                 />
                 <button type="submit">Acessar</button>
                 <Link to="/register">Criar conta gratuita</Link>
-            </form>
+            </Form>
         </>
     );
 };
--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 const SignUp = () => {
+    const handleSubmit = data => {
+        console.tron.log(data);
+    };
+
     return (
         <>
             <img src={logo} alt="GoBarber" />
-            <form action="post">
-                <input name="name" placeholder="Nome Completo" />
-                <input type="email" name="email" placeholder="Seu e-mail" />
-                <input
+            <Form onSubmit={handleSubmit}>
+                <Input name="name" placeholder="Nome Completo" />
+                <Input type="email" name="email" placeholder="Seu e-mail" />
+                <Input
                     type="password"
                     name="password"
                     placeholder="Sua senha"
                 />
                 <button type="submit">Criar Conta</button>
                 <Link to="/">Já tenho login</Link>
-            </form>
+            </Form>
         </>
     );
 };
