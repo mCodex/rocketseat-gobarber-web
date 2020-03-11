85708e76bcdd3eb05e234d8a2fb0ea4672dab766
Mateus Andrade <mat.andrade@live.com>
2020-03-11 19:12:51 -0300

    added auth layout

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 const Signin = () => {
-    return <h1>Login</h1>;
+    return (
+        <>
+            <img src={logo} alt="GoBarber" />
+            <form action="post">
+                <input type="email" name="email" placeholder="Seu e-mail" />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Sua senha"
+                />
+                <button type="submit">Acessar</button>
+                <Link to="/register">Criar conta gratuita</Link>
+            </form>
+        </>
+    );
 };
 
 export default Signin;
--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Signup = () => {
-    return <></>;
+import logo from '~/assets/logo.svg';
+
+const SignUp = () => {
+    return (
+        <>
+            <img src={logo} alt="GoBarber" />
+            <form action="post">
+                <input name="name" placeholder="Nome Completo" />
+                <input type="email" name="email" placeholder="Seu e-mail" />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Sua senha"
+                />
+                <button type="submit">Criar Conta</button>
+                <Link to="/">Já tenho login</Link>
+            </form>
+        </>
+    );
 };
 
-export default Signup;
+export default SignUp;
