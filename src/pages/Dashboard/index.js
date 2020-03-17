316d560f22e26f22bfcc69b94e6bd5f3ac82a6a9
Mateus Andrade <mat.andrade@live.com>
2020-03-17 18:51:35 -0300

    working on dashboard's style

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+import { Container, Time } from './styles';
 
 const Dashboard = () => {
-    return <h1>Dashboard</h1>;
+    return (
+        <Container>
+            <header>
+                <button type="button">
+                    <MdChevronLeft size={36} color="#fff" />
+                </button>
+                <strong>31 de maio</strong>
+                <button type="button">
+                    <MdChevronRight size={36} color="#fff" />
+                </button>
+            </header>
+
+            <ul>
+                <Time past>
+                    <strong>08:00</strong>
+                    <span>Mateus</span>
+                </Time>
+
+                <Time available>
+                    <strong>09:00</strong>
+                    <span>Mateus</span>
+                </Time>
+            </ul>
+        </Container>
+    );
 };
 
 export default Dashboard;
